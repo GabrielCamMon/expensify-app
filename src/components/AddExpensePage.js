a9eeb4c23f60f792c446d4c6da5d828e9dc4e749
Gabriel <gabrielcmdeveloper@outlook.com>
2019-07-22 18:08:44 -0300

    finished router and started redux

---
+++ src/components/AddExpensePage.js
@@ -0,0 +1,3 @@
+import React from "react";
+const AddExpensePage = () => <div>This ii from my add expense component</div>;
+export default AddExpensePage;
---
+++ src/components/HelpPage.js
@@ -0,0 +1,4 @@
+import React from "react";
+const HelpPage = () => <div>This is a Help Page</div>;
+
+export default HelpPage;
