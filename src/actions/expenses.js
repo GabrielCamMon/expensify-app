8eae6e2b416e94b5b1029c93f96f85840716ef93
GabrielCamMon <gabrielcmdeveloper@gmail.com>
2019-08-22 13:57:09 -0300

    Setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -37,6 +37,17 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+// START_REMOVE_EXPENSE
+export const startRemoveExpense = data => {
+  return dispatch => {
+    return database
+      .ref(`expenses/${data.id}`)
+      .remove()
+      .then(() => {
+        dispatch(removeExpense(data));
+      });
+  };
+};
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: "EDIT_EXPENSE",
